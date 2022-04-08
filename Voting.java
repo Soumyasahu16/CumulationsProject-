a19be87b93589a78c5bbafcc0efc5243c31b1188
Soumyasahu16 <102782011+Soumyasahu16@users.noreply.github.com>
2022-04-08 20:58:38 +0530

    Update Voting.java

--- Voting.java
+++ Voting.java
@@ -28,6 +28,21 @@ public class Voting {
 	}
 
 }
+public class NewOne {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Voting v=new Voting();
+		v.takeInput();
+		try {
+		v.verifier();
+			}
+		catch(InvalidAgeException e)
+		{
+			System.out.println(e.getMessage());
+		}
+		}
+
+}
 
 
 
